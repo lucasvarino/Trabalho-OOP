bcfa74e981fa89b0f4bb708af1a8ea3580a3dcf4
lucasvarino <lucasvarino@gmail.com>
2022-07-30 21:04:43 -0300

    Adicionando escrita no JSON na parte de usuários

--- PokedexOO/src/main/java/com/mycompany/interfacepokedex/UserInterface.java
+++ PokedexOO/src/main/java/com/mycompany/interfacepokedex/UserInterface.java
@@ -2,6 +2,7 @@ package com.mycompany.interfacepokedex;
 
 
 import com.mycompany.pokedexoo.pokemon.Pokemon;
+import com.mycompany.pokedexoo.users.Jogador;
 import com.mycompany.pokedexoo.users.Treinador;
 import com.mycompany.pokedexoo.users.Usuario;
 import java.io.IOException;
@@ -29,25 +30,20 @@ public class UserInterface {
         System.out.println("");
 
         //InterfaceInicial interfaceInicial = new InterfaceInicial();
-        InterfaceRegistros interfaceRegistros = new InterfaceRegistros();
+        //InterfaceRegistros interfaceRegistros = new InterfaceRegistros();
         
         Scanner teclado = new Scanner(System.in);        
 
         System.out.println("Digite seu nome: ");
         String nome = teclado.nextLine();
-
-        System.out.println("");
-
-        Pokemon pokemon = Pokemon.getPokemonByUrl(nome);
         
-        System.out.println("Pokemon 2");
-        String nome2 = teclado.nextLine();
+        System.out.println("Digite sua senha: ");
+        String senha = teclado.nextLine();
         
-        Pokemon pokemon2 = Pokemon.getPokemonByUrl(nome2);
+        Jogador jogador = new Jogador(nome, senha);
         
-        pokemon.imprimePokemon();
-        System.out.println("");
-        pokemon2.imprimePokemon();
+        Treinador treinador = new Treinador("Ash2", "Sinnoh");
+        jogador.setTreinador(treinador);
         
 
     }
--- PokedexOO/src/main/java/com/mycompany/pokedexoo/users/Jogador.java
+++ PokedexOO/src/main/java/com/mycompany/pokedexoo/users/Jogador.java
@@ -4,8 +4,11 @@
  */
 package com.mycompany.pokedexoo.users;
 
+import com.google.gson.Gson;
 import com.mycompany.pokedexoo.users.Usuario;
 import com.mycompany.pokedexoo.users.Treinador;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -16,18 +19,20 @@ import java.util.*;
 public class Jogador extends Usuario {
     private ArrayList<Treinador> treinadores;
     
-    public Jogador(String _nome, String _senha) {
+    public Jogador(String _nome, String _senha) throws IOException {
         super(_nome, _senha);
         this.treinadores = new ArrayList<>();
         this.treinadores.add(new Treinador("Ash", "Kanto")); // Por padrão o Jogador começa com um treinador
+        this.salvarJogadorJson();
     }
     
     public ArrayList<Treinador> getTreinadores() {
         return treinadores;
     }
 
-    public void setTreinador(Treinador treinador) {
+    public void setTreinador(Treinador treinador) throws IOException {
         this.treinadores.add(treinador);
+        this.salvarJogadorJson();
     }
     
     public Treinador getTreinadorById(int id) {
@@ -43,5 +48,12 @@ public class Jogador extends Usuario {
         for (Treinador treinador : treinadores) {
             System.out.println(treinador.getNome());
         }
+    }
+    
+    private void salvarJogadorJson() throws IOException {
+        Gson gson = new Gson();
+        FileWriter writter = new FileWriter("users.json");
+        gson.toJson(this, writter);
+        writter.close();
     }
 }
